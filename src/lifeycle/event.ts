937dd17025aa33263b5fee0bee3d5e5742d140c2
zouzhiqiang <465633678@qq.com>
2020-11-22 17:11:44 +0800

    更新

--- src/lifeycle/event.ts
+++ src/lifeycle/event.ts
@@ -1,6 +1,8 @@
 // 事件处理
 import Vue from "..";
-
+import { elements } from "../render/render";
+// canvas需要监听的事件
+let canvasEvents = ['touchstart', 'touchmove', 'touchend', 'click']
 /**
  * 初始化事件
  * @param vm vue对象
@@ -13,5 +15,13 @@ export function initEvent(vm: Vue) {
  * @param vm vue对象
  */
 function initCanvasEvent(vm: Vue) {
-  
+  // 获取canvas元素
+  const canvas: HTMLCanvasElement = vm.$options.el as HTMLCanvasElement
+  canvasEvents.forEach(event => {
+    canvas.addEventListener(event, () => {
+      elements.forEach((ele: any) => {
+        // ele.$emit(event)
+      })
+    })
+  })
 }
--- src/render/render.ts
+++ src/render/render.ts
@@ -4,14 +4,16 @@ import { createElement, VueElement } from "./element"
 import * as componets from './components'
 import Vue from "../vue"
 const renderComponents: Record<string, any> = componets
+// 保存elements
+export let elements: any = []
 /**
  * 渲染方法
  * @param {VueElement} element 元素
  */
 export function render(vm: Vue, element: VueElement) {
+  elements = []
   // 渲染组件
   componentsRender(vm, element)
-  
 }
 /**
  * 渲染方法
@@ -25,6 +27,7 @@ export function render(vm: Vue, element: VueElement) {
 function componentsRender(vm: Vue, element: VueElement) {
   const renderComponentInstance = getComponentInstance(vm, element)
   renderComponentInstance.render()
+  elements.push(renderComponentInstance)
 }
 /**
  * 获取元素实例
